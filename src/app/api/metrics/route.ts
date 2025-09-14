a5c30dfaddb822e27c4ce081ea5bdb1e6d6ec687
Ainesh Sridhar <aineshsridhar@gmail.com>
2025-09-14 21:21:05 +0530

    Updated commands for prometheus

---
+++ src/app/api/metrics/route.ts
@@ -0,0 +1,12 @@
+import client from 'prom-client';
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+export async function GET(req: Request) {
+  const metrics = await client.register.metrics();
+  return new Response(metrics, {
+    status: 200,
+    headers: { 'Content-Type': client.register.contentType },
+  });
+}
